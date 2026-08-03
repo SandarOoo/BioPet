b484349270f49014746d65b086f13aad64755399
SandarOoo <sandaroo2232005@gmail.com>
2026-08-04 00:06:06 +0630

    update add product

--- backend/routes/business.js
+++ backend/routes/business.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const { protect } = require("../middleware/auth");
+const upload = require("../middleware/upload");
 
 const {
   acceptAgreement,
